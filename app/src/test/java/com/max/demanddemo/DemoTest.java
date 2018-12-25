0a8e4081918371ecb13cea44ecc99833ec03fd41
shixin <shixin58@163.com>
2018-12-25 17:51:45 +0800

    Java单元测试

---
+++ app/src/test/java/com/max/demanddemo/DemoTest.java
@@ -0,0 +1,15 @@
+package com.max.demanddemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>Created by shixin on 2018/12/25.
+ */
+public class DemoTest {
+    @Test
+    public void nameFormatRight() {
+        assertEquals(5, 5);
+    }
+}
