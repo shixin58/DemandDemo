378f63230a55514cef424ad5386929e27fc07b30
shixin <shixin58@163.com>
2019-02-11 10:41:30 +0800

    native scheme跳转

--- app/src/main/java/com/max/demanddemo/MainActivity.java
+++ app/src/main/java/com/max/demanddemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 
 import java.net.URISyntaxException;
@@ -33,12 +34,19 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.tv_invoke_bdvideo:
                 String urlString = "bdvideo://invoke?#Intent;action=com.baidu.video.search;S.action=special;S.title=aaa;S.refer=rbbhwdc;S.tomain=0;end";
+
                 String urlString2 = "bdvideo://invoke?#Intent;action=com.baidu.video.channel;end";
-                String urlString3 = "#Intent;action=action_oppo_push_to_video_activity;category=bdvideo;end";
-                String urlString4 = "#Intent;action=action_current_day_notification_num_insufficient;category=bdvideo;end";
+
+                String urlString3 = "#Intent;action=action_current_day_notification_num_insufficient;category=bdvideo;end";
+
+                String packageName = null;
+                packageName = "com.baidu.video";
+
                 try {
-                    intent = Intent.parseUri(urlString4, 0);
-                    intent.setPackage("com.baidu.video");
+                    intent = Intent.parseUri(urlString3, 0);
+                    if (!TextUtils.isEmpty(packageName)) {
+                        intent.setPackage("com.baidu.video");
+                    }
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
                 } catch (URISyntaxException e) {
@@ -48,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 /*intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(urlString));
-                intent.setPackage("com.baidu.video");
+                intent.setPackage(packageName);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);*/
                 break;
