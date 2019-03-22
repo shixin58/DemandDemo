594287d613953d68e62eeaf2a178ab6fe248ad19
shixin <shixin58@163.com>
2019-03-22 23:36:12 +0800

    add jitpack_lib

---
+++ jitpack_lib/src/main/java/com/max/jitpack_lib/BattleClient.java
@@ -0,0 +1,14 @@
+package com.max.jitpack_lib;
+
+/**
+ * <p>Created by shixin on 2019/3/22.
+ */
+public class BattleClient {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the real world");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
