22d2b82828e5e22fb295b1eec0ef5071e1c84a19
shixin <shixin58@163.com>
2019-03-23 10:38:31 +0800

    add compiler

---
+++ compiler/src/main/java/com/max/compiler/SunnyClient.java
@@ -0,0 +1,18 @@
+package com.max.compiler;
+
+/**
+ * <p>Created by shixin on 2019/3/23.
+ */
+public class SunnyClient {
+    public static void main(String[] args) {
+        System.out.println("Smart boy!");
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
